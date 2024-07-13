3a93cf12cc64a3a43c9f74a29e9f1d588feaaa74
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-07-13 18:00:13 +0100

    0x02 minOperations corr

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,12 +5,14 @@
 def minOperations(n: int) -> int:
     """ calculates the fewest number of operations needed
     to result in exactly n H characters in the file """
-    if n <= 1:
+    if n < 2:
         return 0
-    for i in range(n - 1, 0, -1):
-        if n % i == 0:
+    i = n
+    for j in range(2, n // 2):
+        if n % j == 0:
+            i = j
             break
-    if i == 1:
+    if i == n:
         return n
     else:
-        return (n // i) + minOperations(i)
+        return (i) + minOperations(n // i)
