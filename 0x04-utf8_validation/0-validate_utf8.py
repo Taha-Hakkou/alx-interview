ce29c437809dad53623e08fa7e8f1b5167753ce8
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-07-13 15:42:36 +0100

    0x04 utf8_validation corr3

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,28 +5,23 @@ from typing import List
 
 def validUTF8(data: List[int]) -> bool:
     """ determines if a given data set represents a valid UTF-8 encoding """
+    for i in range(len(data)):
+        data[i] = data[i] & 255
     while len(data) > 0:
-        data[0] = data[0] & 255
         if data[0] < 128:
             data = data[1:]
+        elif data[0] >= 192 and data[0] < 224:
+            if len(data) == 1 or data[1] < 128 or data[1] >= 192:
+                return False
+            data = data[2:]
+        elif data[0] >= 224 and data[0] < 240:
+            if len(data) < 3 or any([b < 128 or b >= 192 for b in data[1:3]]):
+                return False
+            data = data[3:]
+        elif data[0] >= 240 and data[0] < 248:
+            if len(data) < 4 or any([b < 128 or b >= 192 for b in data[1:4]]):
+                return False
+            data = data[4:]
         else:
-            data[1] = data[1] & 255
-            if data[0] >= 192 and data[0] < 224:
-                if len(data) == 1 or data[1] < 128 or data[1] >= 192:
-                    return False
-                data = data[2:]
-            else:
-                data[2] = data[2] & 255
-                if data[0] >= 224 and data[0] < 240:
-                    if len(data) < 3 or any([b < 128 or b >= 192 for b in data[1:3]]):
-                        return False
-                    data = data[3:]
-                else:
-                    data[3] = data[3] & 255
-                    if data[0] >= 240 and data[0] < 248:
-                        if len(data) < 4 or any([b < 128 or b >= 192 for b in data[1:4]]):
-                            return False
-                        data = data[4:]
-                    else:
-                        return False
+            return False
     return True
