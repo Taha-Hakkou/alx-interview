2f259ec2cc6f2d5187361e744d20c6aac9538bad
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-07-17 11:20:42 +0100

    0x07 rotate_2d_matrix done

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 0-rotate_2d_matrix """
+
+
+def rotate_2d_matrix(matrix):
+    """ Rotates an n * n matrix 90 degrees clockwise """
+    n = len(matrix)
+    for i in range(n // 2):
+        tmp = [matrix[j][n - 1 - i] for j in range(i, n - 1 - i)]
+        for j in range(i, n - 1 - i):
+            matrix[j][n - 1 - i] = matrix[i][j]
+        tmp2 = matrix[n - 1 - i][1 + i: n - i]
+        for j in range(1 + i, n - i):
+            matrix[n - 1 - i][j] = tmp[-j]
+        tmp = [matrix[j][i] for j in range(n - 1 - i, i, -1)]
+        for j in range(1 + i, n - i):
+            matrix[j][i] = tmp2[j - 1]
+        for j in range(i, n - 1 - i):
+            matrix[i][j] = tmp[j - i]
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,7 @@
+# 0x07-rotate_2d_matrix
+
+Rotate 2D Matrix Project
+
+## Tasks:
+
+0-rotate_2d_matrix.py
