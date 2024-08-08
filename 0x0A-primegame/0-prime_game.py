befded19e27184707b6a19d8e375bce9229c7257
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-08-08 13:04:14 +0100

    0x0A prime_game corr2

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,22 +2,20 @@
 """ 0-prime_game """
 
 
-def isWinner(x, nums):
+def isWinner(x: int, nums: list[int]) -> str:
     """ Returns the winner of the PrimeGame """
     scores = {'Maria': 0, 'Ben': 0}
     for n in nums:
         if x == 0:
             break
-        n_array = {i for i in range(1, n + 1)}
+        n_array = [i for i in range(1, n + 1)]
         essays = 0
-        while n_array != {1}:
-            prime = list(n_array)[1]
-            n_array = {i for i in n_array if i % prime != 0}
+        while n_array != [1]:
+            prime = n_array[1]
+            n_array = [i for i in n_array if i % prime != 0]
             essays += 1
-        if essays % 2 == 0:
-            scores['Ben'] += 1
-        else:
-            scores['Maria'] += 1
+        round_winner = 'Ben' if essays % 2 == 0 else 'Maria'
+        scores[round_winner] += 1
         x -= 1
     if scores['Maria'] > scores['Ben']:
         return 'Maria'
